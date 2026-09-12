ff0b03bca5695003a4cc4a5d3db68e461a738c7c
Chris Melvin <cmelvin215@gmail.com>
2026-09-12 14:40:40 +0900

    Add GitHub Actions workflow to build and push image to ECR

---
+++ terraform/github-oidc.tf
@@ -0,0 +1,72 @@
+variable "github_repo" {
+  description = "GitHub repository allowed to assume the role, as owner/name"
+  type        = string
+  default     = "chris-git-321/react-resume-platform"
+}
+
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+data "aws_iam_policy_document" "github_assume" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [data.aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${var.github_repo}:*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = "${var.project_name}-github-actions"
+  assume_role_policy = data.aws_iam_policy_document.github_assume.json
+}
+
+data "aws_iam_policy_document" "ecr_push" {
+  statement {
+    sid       = "GetAuthToken"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid = "PushToRepository"
+
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:CompleteLayerUpload",
+      "ecr:InitiateLayerUpload",
+      "ecr:PutImage",
+      "ecr:UploadLayerPart",
+    ]
+
+    resources = [
+      "arn:aws:ecr:${var.region}:${data.aws_caller_identity.current.account_id}:repository/${var.project_name}",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "ecr_push" {
+  name   = "${var.project_name}-ecr-push"
+  role   = aws_iam_role.github_actions.id
+  policy = data.aws_iam_policy_document.ecr_push.json
+}
+
+output "github_actions_role_arn" {
+  description = "Role assumed by GitHub Actions via OIDC"
+  value       = aws_iam_role.github_actions.arn
+}
